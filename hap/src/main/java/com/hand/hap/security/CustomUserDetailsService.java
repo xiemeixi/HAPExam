a630b7414f08089f6c7b1b7ad13a351fdf7c33c9
周升洋 <shengyang.zhou@hand-china.com>
2016-06-12 21:43:36 +0800

    update spring security

--- hap/src/main/java/com/hand/hap/security/CustomUserDetailsService.java
+++ hap/src/main/java/com/hand/hap/security/CustomUserDetailsService.java
@@ -1,27 +1,20 @@
 package com.hand.hap.security;
 
-import com.hand.hap.account.dto.User;
-import com.hand.hap.account.exception.UserException;
-import com.hand.hap.account.service.IUserService;
-import com.hand.hap.security.captcha.ICaptchaManager;
-import org.apache.commons.lang.StringUtils;
+import java.util.ArrayList;
+import java.util.Collection;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.StandardPasswordEncoder;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
-import org.springframework.web.util.WebUtils;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
-import java.util.Collection;
+import com.hand.hap.account.dto.User;
+import com.hand.hap.account.service.IUserService;
+import com.hand.hap.security.captcha.ICaptchaManager;
 
 /**
  * Created by hailor on 16/6/12.
@@ -37,24 +30,23 @@ class CustomUserDetailsService implements UserDetailsService {
     private ICaptchaManager captchaManager;
 
     @Autowired
-    private StandardPasswordEncoder passwordManager;
+    private PasswordEncoder passwordManager;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userService.selectByUserName(username);
-        if (user==null)
-        {
-            throw new UsernameNotFoundException("User not found:"+username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found:" + username);
         }
-        Collection<SimpleGrantedAuthority> authorities=new ArrayList<SimpleGrantedAuthority>();
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<SimpleGrantedAuthority>();
         authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
         authorities.add(new SimpleGrantedAuthority("ADMIN"));
         authorities.add(new SimpleGrantedAuthority("ROLE_UNITY"));
 
-        UserDetails userDetails = new CustomUserDetails(user.getUserId(),user.getUserName(),user.getPasswordEncrypted(),true,true,true,true,authorities);
+        UserDetails userDetails = new CustomUserDetails(user.getUserId(), user.getUserName(),
+                user.getPasswordEncrypted(), true, true, true, true, authorities);
         logger.error(passwordManager.encode(username));
         return userDetails;
     }
-
 
 }
--- hap/src/main/java/com/hand/hap/security/PermissionVoter.java
+++ hap/src/main/java/com/hand/hap/security/PermissionVoter.java
@@ -1,19 +1,38 @@
 package com.hand.hap.security;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.AccessDecisionVoter;
 import org.springframework.security.access.ConfigAttribute;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.FilterInvocation;
 
-import java.util.Collection;
+import com.hand.hap.account.dto.Role;
+import com.hand.hap.cache.Cache;
+import com.hand.hap.cache.CacheManager;
+import com.hand.hap.cache.impl.RoleResourceCache;
+import com.hand.hap.core.BaseConstants;
+import com.hand.hap.function.dto.Resource;
 
 /**
  * Created by hailor on 16/6/12.
  */
 public class PermissionVoter implements AccessDecisionVoter<FilterInvocation> {
     private static final Logger logger = LoggerFactory.getLogger(PermissionVoter.class);
+
+    @Autowired
+    private RoleResourceCache roleResourceCache;
+
+    @Autowired
+    private CacheManager cacheManager;
 
     @Override
     public boolean supports(ConfigAttribute attribute) {
@@ -31,9 +50,50 @@ public class PermissionVoter implements AccessDecisionVoter<FilterInvocation> {
         assert authentication != null;
         assert fi != null;
         assert attributes != null;
-        logger.error(fi.getRequest().getRequestURI());
-        //取得当前的Request
-        //result = ACCESS_DENIED;
+        HttpServletRequest request = fi.getRequest();
+        String uri = StringUtils.substringAfter(request.getRequestURI(), request.getContextPath());
+        if (uri.startsWith("/")) {
+            uri = uri.substring(1);
+        }
+        if ("".equalsIgnoreCase(uri)) {
+            return ACCESS_ABSTAIN;
+        }
+        Cache<Resource> cache = cacheManager.getCache(BaseConstants.CACHE_RESOURCE_URL);
+        Resource resource = cache.getValue(uri);
+        if (resource == null) {
+            // logger.warn("url:'{}' is not registered", uri);
+            return ACCESS_ABSTAIN;
+            // return ACCESS_DENIED;
+        }
+        if (!BaseConstants.YES.equalsIgnoreCase(resource.getLoginRequire())) {
+            if (logger.isDebugEnabled()) {
+                logger.debug("url :'{}' does not require login.", uri);
+            }
+            return ACCESS_ABSTAIN;
+        }
+        if (!BaseConstants.YES.equalsIgnoreCase(resource.getAccessCheck())) {
+            if (logger.isDebugEnabled()) {
+                logger.debug("url :'{}' need no access control.", uri);
+            }
+            return ACCESS_ABSTAIN;
+        }
+
+        HttpSession session = request.getSession();
+        if (session == null) {
+            return ACCESS_DENIED;
+        }
+        Long roleId = (Long) session.getAttribute(Role.FIELD_ROLE_ID);
+        if (roleId == null) {
+            return ACCESS_DENIED;
+        }
+
+        Long[] accessible = roleResourceCache.getValue(String.valueOf(roleId));
+        if (accessible == null || !Arrays.asList(accessible).contains(resource.getResourceId())) {
+            logger.warn("access to uri :'{}' denied.", uri);
+            return ACCESS_DENIED;
+        }
+        // 取得当前的Request
+        // result = ACCESS_DENIED;
         result = ACCESS_ABSTAIN;
 
         return result;
