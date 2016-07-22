72327b7bf399bcc76c3c5f937140ad9ebc0986be
周升洋 <shengyang.zhou@hand-china.com>
2016-07-22 19:22:36 +0800

    [IMP] resource security check now support url with {path variable}

--- hap/src/main/java/com/hand/hap/security/PermissionVoter.java
+++ hap/src/main/java/com/hand/hap/security/PermissionVoter.java
@@ -2,18 +2,30 @@ package com.hand.hap.security;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.BeanFactoryUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.AccessDecisionVoter;
 import org.springframework.security.access.ConfigAttribute;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.FilterInvocation;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.FrameworkServlet;
+import org.springframework.web.servlet.HandlerMapping;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 import com.hand.hap.account.dto.Role;
 import com.hand.hap.cache.Cache;
@@ -28,11 +40,17 @@ import com.hand.hap.function.dto.Resource;
 public class PermissionVoter implements AccessDecisionVoter<FilterInvocation> {
     private static final Logger logger = LoggerFactory.getLogger(PermissionVoter.class);
 
+    private static String APP_SERVLET_CONTEXT_KEY = FrameworkServlet.SERVLET_CONTEXT_PREFIX + "appServlet";
+
     @Autowired
     private RoleResourceCache roleResourceCache;
 
     @Autowired
     private CacheManager cacheManager;
+
+    private Cache<Resource> resourceCache;
+
+    private RequestMappingHandlerMapping requestMappingHandlerMapping;
 
     @Override
     public boolean supports(ConfigAttribute attribute) {
@@ -44,24 +62,100 @@ public class PermissionVoter implements AccessDecisionVoter<FilterInvocation> {
         return true;
     }
 
+    private void initRequestMappingHandlerMapping(HttpServletRequest request) {
+        if (requestMappingHandlerMapping != null) {
+            return;
+        }
+        ServletContext servletContext = request.getServletContext();
+
+        WebApplicationContext appContext = WebApplicationContextUtils.getWebApplicationContext(servletContext,
+                APP_SERVLET_CONTEXT_KEY);
+        Map<String, HandlerMapping> allRequestMappings = BeanFactoryUtils.beansOfTypeIncludingAncestors(appContext,
+                HandlerMapping.class, true, false);
+        for (HandlerMapping handlerMapping : allRequestMappings.values()) {
+            if (handlerMapping instanceof RequestMappingHandlerMapping) {
+                requestMappingHandlerMapping = (RequestMappingHandlerMapping) handlerMapping;
+                break;
+            }
+        }
+
+    }
+
+    private String getBestMatchPattern(HttpServletRequest request, String uri) {
+        initRequestMappingHandlerMapping(request);
+        Map<RequestMappingInfo, HandlerMethod> handlerMethods = requestMappingHandlerMapping.getHandlerMethods();
+        for (Map.Entry<RequestMappingInfo, HandlerMethod> entry : handlerMethods.entrySet()) {
+            RequestMappingInfo requestMappingInfo = entry.getKey();
+            List<String> patterns = requestMappingInfo.getPatternsCondition().getMatchingPatterns(uri);
+            if (patterns.size() > 0) {
+                return patterns.get(0);
+            }
+        }
+        return uri;
+    }
+
+    private Resource getResourceOfUri(HttpServletRequest request, String uri_ori, String uri_trim) {
+        if (resourceCache == null) {
+            resourceCache = cacheManager.getCache(BaseConstants.CACHE_RESOURCE_URL);
+        }
+        Resource resource = resourceCache.getValue(uri_trim);
+        if (resource == null) {
+            if (logger.isWarnEnabled()) {
+                logger.warn("url {} is not registered", uri_trim);
+            }
+            // 所有的 html 都是通过 pattern 映射的,但是这些 pattern 都没有注册
+            // 每个 html 的路径都是单独注册,对于一个 html url,如果在 cache 中没找到,说明这个 url 没有注册
+            // 这种情况,没必要再去解析 这个 url 对应哪个 pattern
+            if (!StringUtils.endsWith(uri_ori, ".html")) {
+                String pattern = getBestMatchPattern(request, uri_ori);
+                if (Objects.equals(pattern, uri_ori)) {
+                    // pattern and uri_ori are the same
+                    return null;
+                }
+                if (logger.isDebugEnabled()) {
+                    logger.debug("{} match pattern {}", uri_ori, pattern);
+                }
+                if (pattern.startsWith("/")) {
+                    pattern = pattern.substring(1);
+                }
+                resource = resourceCache.getValue(pattern);
+                if (resource == null) {
+                    if (logger.isWarnEnabled()) {
+                        logger.warn("pattern {} is not registered", pattern);
+                    }
+                }
+            }
+        }
+
+        return resource;
+    }
+
     @Override
     public int vote(Authentication authentication, FilterInvocation fi, Collection<ConfigAttribute> attributes) {
         int result = ACCESS_ABSTAIN;
         assert authentication != null;
         assert fi != null;
         assert attributes != null;
+
+        // 已经 permitAll 的 url 不再过滤(主要是一些资源类 url,通用 url)
+        for (ConfigAttribute attribute : attributes) {
+            if ("permitAll".equals(attribute.toString())) {
+                return result;
+            }
+        }
+
         HttpServletRequest request = fi.getRequest();
         String uri = StringUtils.substringAfter(request.getRequestURI(), request.getContextPath());
+        String uri_ori = uri;
+
         if (uri.startsWith("/")) {
             uri = uri.substring(1);
         }
-//        if ("".equals(uri)) {
-//            return ACCESS_DENIED;
-//        }
-        Cache<Resource> cache = cacheManager.getCache(BaseConstants.CACHE_RESOURCE_URL);
-        Resource resource = cache.getValue(uri);
+        if ("".equals(uri)) {
+            return ACCESS_ABSTAIN;
+        }
+        Resource resource = getResourceOfUri(request, uri_ori, uri);
         if (resource == null) {
-            // logger.warn("url:'{}' is not registered", uri);
             return ACCESS_ABSTAIN;
             // return ACCESS_DENIED;
         }
@@ -92,8 +186,6 @@ public class PermissionVoter implements AccessDecisionVoter<FilterInvocation> {
             logger.warn("access to uri :'{}' denied.", uri);
             return ACCESS_DENIED;
         }
-        // 取得当前的Request
-        // result = ACCESS_DENIED;
         result = ACCESS_ABSTAIN;
 
         return result;
